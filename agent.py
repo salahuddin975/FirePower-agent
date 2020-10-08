8cfaec40114cf68945ba39f83aee10ee9640ab7b
salahuddin975 <salahuddin975@gmail.com>
2020-10-08 17:59:42 -0500

    Replay buffer partially done.

--- agent.py
+++ agent.py
@@ -10,6 +10,68 @@ gym.logger.set_level(40)
 
 
 
+class Memory:
+    def __init__(self, state_spaces, action_spaces, buffer_capacity=100000, batch_size=64):
+        self.counter = 0
+        self.capacity = buffer_capacity
+        self.batch_size = batch_size
+
+        # current state buffer
+        self.st_bus =np.zeros((state_spaces[0]))
+        self.st_branch =np.zeros((state_spaces[1]))
+        self.st_fire =np.zeros((state_spaces[2], state_spaces[2]))
+        self.st_generator_output =np.zeros((state_spaces[3]))
+        self.st_load_demand =np.zeros((state_spaces[4]))
+        self.st_theta =np.zeros((state_spaces[5]))
+        self.state = np.array((self.st_bus, self.st_branch, self.st_fire, self.st_generator_output, self.st_load_demand, self.st_theta))
+        self.states = np.array([self.state]*self.capacity)
+
+        # action buffer
+        self.act_bus = np.zeros((action_spaces[0]))
+        self.act_branch = np.zeros((action_spaces[1]))
+        self.act_generator_selector = np.zeros((action_spaces[2]))
+        self.act_generator_injection = np.zeros((action_spaces[3]))
+        self.action = np.array((self.act_bus, self.act_branch, self.act_generator_selector, self.act_generator_injection))
+        self.actions = np.array([self.action]*self.capacity)
+
+        # reward buffer
+        self.rewards = np.zeros((self.capacity, 1))
+
+        # next state buffer
+        self.next_states = np.array([self.state]*self.capacity)
+
+
+    def add_record(self, record):
+        index = self.counter % self.capacity
+
+        self.states[index][0] = np.copy(record[0]["bus_status"])
+        self.states[index][1] = np.copy(record[0]["branch_status"])
+        self.states[index][2] = np.copy(record[0]["fire_state"])
+        self.states[index][3] = np.copy(record[0]["generator_injection"])
+        self.states[index][4] = np.copy(record[0]["load_demand"])
+        self.states[index][5] = np.copy(record[0]["theta"])
+
+        self.actions[index][0] = np.copy(record[1]["bus_status"])
+        self.actions[index][1] = np.copy(record[1]["branch_status"])
+        self.actions[index][2] = np.copy(record[1]["branch_status"])
+        self.actions[index][3] = np.copy(record[1]["branch_status"])
+
+        self.rewards[index] = record[2]
+
+        self.next_states[index][0] = np.copy(record[3]["bus_status"])
+        self.next_states[index][1] = np.copy(record[3]["branch_status"])
+        self.next_states[index][2] = np.copy(record[3]["fire_state"])
+        self.next_states[index][3] = np.copy(record[3]["generator_injection"])
+        self.next_states[index][4] = np.copy(record[3]["load_demand"])
+        self.next_states[index][5] = np.copy(record[3]["theta"])
+
+        self.counter = self.counter + 1
+
+
+
+
+
+
 def get_actor(state_space, action_space):
     # bus -> MultiBinary(24)
     bus_input = layers.Input(shape=(state_space[0],))
@@ -174,7 +236,7 @@ def get_state_spaces(env):
     num_load_demand = observation_space["load_demand"].shape[0]
     num_theta = observation_space["theta"].shape[0]
     state_spaces = [num_st_bus, num_st_branch, num_fire_status, num_gen_output, num_load_demand, num_theta]
-    print(f"State Spaces: num bus: {num_st_bus}, num branch: {num_st_branch}, fire status: {num_fire_status}, num_gen_injection: {num_gen_output}, num_load_demand: {num_load_demand}, num_theta: {num_theta}")
+    # print(f"State Spaces: num bus: {num_st_bus}, num branch: {num_st_branch}, fire status: {num_fire_status}, num_gen_injection: {num_gen_output}, num_load_demand: {num_load_demand}, num_theta: {num_theta}")
 
     return state_spaces
 
@@ -186,7 +248,7 @@ def get_action_spaces(env):
     num_generator_selector = action_space["generator_selector"].shape[0]
     num_generator_injection = action_space["generator_injection"].shape[0]
     action_spaces = [num_bus, num_branch, num_generator_selector, num_generator_injection]
-    print (f"Action Spaces: num bus: {num_bus}, num branch: {num_branch}, num_generator_selector: {num_generator_selector}, num generator injection: {num_generator_injection}")
+    # print (f"Action Spaces: num bus: {num_bus}, num branch: {num_branch}, num_generator_selector: {num_generator_selector}, num generator injection: {num_generator_injection}")
 
     return action_spaces
 
@@ -206,11 +268,15 @@ def main(args):
     tf_state = get_tf_state(state)
     actor_action = actor(tf_state)
     action = get_np_action(actor_action)
-    print ("action: ", action)
+    # print ("action: ", action)
 
     tf_critic_input = get_tf_critic_input(state, action)
     critic_reward = critic(tf_critic_input)
-    print("critic_reward: ", critic_reward)
+    # print("critic_reward: ", critic_reward)
+
+    memory = Memory(state_spaces, action_spaces, 5, 64)
+    print("action: ", action)
+    memory.add_record((state, action, 1, state))
 
 
     # action = env.action_space.sample()

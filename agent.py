eb28ba63909d8520fc67d947c903f4507456e5fa
salahuddin975 <salahuddin975@gmail.com>
2020-12-11 21:43:07 -0600

    remove load demand; add hidden layer

--- agent.py
+++ agent.py
@@ -120,9 +120,10 @@ class Agent:
         # line_flow -> Box(34, )
         line_flow_input = layers.Input(shape=(self._state_spaces[6], ))
 
-        state = layers.Concatenate() ([bus_input, branch_input, fire_distance_input, gen_inj_input, load_demand_input, theta_input, line_flow_input])
+        state = layers.Concatenate() ([bus_input, branch_input, fire_distance_input, gen_inj_input, theta_input, line_flow_input])
         # state = layers.Concatenate() ([bus_input1, branch_input1, fire_distance_input1, gen_inj_input1, load_demand_input1, theta_input1])
         hidden = layers.Dense(512, activation="tanh") (state)
+        hidden = layers.Dense(256, activation="tanh") (hidden)
         hidden = layers.Dense(128, activation="tanh") (hidden)
 
         # bus -> MultiBinary(24)
@@ -179,12 +180,13 @@ class Agent:
         # act_gen_injection1 = layers.Dense(32, activation="relu") (act_gen_injection)          # power ramping up/down
 
         # state = layers.Concatenate() ([st_bus, act_gen_injection])
-        state = layers.Concatenate() ([st_bus, st_branch, st_fire_distance, st_gen_output, st_load_demand, st_theta, st_line_flow,
+        state = layers.Concatenate() ([st_bus, st_branch, st_fire_distance, st_gen_output, st_theta, st_line_flow,
                                        act_bus, act_branch, act_gen_injection])
         # state = layers.Concatenate() ([st_bus1, st_branch1, st_fire_distance1, st_gen_output1, st_load_demand1, st_theta1,
         #                                act_bus1, act_branch1, act_gen_injection1])
 
         hidden = layers.Dense(512, activation="relu") (state)
+        hidden = layers.Dense(256, activation="relu") (hidden)
         hidden = layers.Dense(128, activation="relu") (hidden)
         reward = layers.Dense(1, activation="linear") (hidden)
 

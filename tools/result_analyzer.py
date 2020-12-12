c6ae8fa0b6375860e18a22f4ecd94b828a101b14
salahuddin975 <salahuddin975@gmail.com>
2020-12-11 21:32:58 -0600

    update result analyzer

--- tools/result_analyzer.py
+++ tools/result_analyzer.py
@@ -1,47 +1,35 @@
-import sys
-import csv
+import pandas as pd
 
-# seed = int(sys.argv[1])
 
-seed=101
-num_of_result=20
+seed = 101
+num_of_result = 25
 path = "rnslab2/"
 
-file_name = "fire_power_reward_list_v0.csv"
 test_result = []
+file_name = "fire_power_reward_list_v0.csv"
 
 for i in range(num_of_result):
-    file_path = path + "seed_" + str(seed+i) + "_" + file_name
+    file_path = path + "seed_" + str(seed + i) + "_" + file_name
     print(file_path)
 
-    with open(file_path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    df = pd.read_csv(file_path, header=0)
 
-        penalty_ep_no = 0
-        min_penalty = -2700000.00
-        step_ep_no = 0
-        max_step = 0
+    penalty_ep_no = 0
+    min_penalty = -2700000.00
+    step_count = 0
 
-        prev_row = ""
-        flag = False
-        for row in csv_reader:
-            if flag:
-                if int(row[1])%20 == 2 and int(row[1]) > 50:
-                    if (float(row[3]) + float(prev_row[3]))/2 > min_penalty:
-                        penalty_ep_no = row[1]
-                        min_penalty = (float(row[3]) + float(prev_row[3]))/2
-                        print("min_penalty: ", min_penalty, "; at: ", penalty_ep_no)
+    for i in range(len(df)):
+        if i >= 50 and df.loc[i, "episode_number"] % 20 == 0 and i+4 < len(df):
+            penalty = (df.loc[i+1, "reward"] + df.loc[i+2, "reward"] + df.loc[i+3, "reward"] + df.loc[i+4, "reward"]) / 4
+            if min_penalty < penalty:
+                min_penalty = penalty
+                penalty_ep_no = i
+                step_count = min(df.loc[i+1, "max_reached_step"], df.loc[i+2, "max_reached_step"], df.loc[i+3, "max_reached_step"], df.loc[i+4, "max_reached_step"])
+                print("min_penalty: ", min_penalty, "; at: ", penalty_ep_no)
 
-                if (int(row[2]) >= max_step) and (int(row[1])%20 == 1 or int(row[1])%20 == 2) and int(row[1]) > 50:
-                        step_ep_no = row[1]
-                        max_step = int(row[2])
+    test_result.append((penalty_ep_no, step_count, min_penalty))
+    print("episode_no: ", penalty_ep_no, "; max_reached_step: ", step_count, "; minimum_penalty: ", min_penalty)
 
-            flag = True
-            prev_row = row
-
-        test_result.append((penalty_ep_no, max_step, min_penalty))
-        print("max_reached_step_episode: ", step_ep_no)
-        print("episode_no: ", penalty_ep_no, "; max_reached_step: ", max_step, "; minimum_penalty: ", min_penalty)
 
 penalty = []
 for i, result in enumerate(test_result):
@@ -49,4 +37,3 @@ for i, result in enumerate(test_result):
     penalty.append(result[2])
 
 print(f"\n - Summary: average minimum penalty: {sum(penalty)/5}; lowest: {max(penalty)}; highest: {min(penalty)}")
-

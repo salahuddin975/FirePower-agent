1a5f3f079bf6611e39fda33222046335fc102b2c
salahuddin975 <salahuddin975@gmail.com>
2020-12-13 21:55:57 -0600

    add binary crawler

---
+++ tools/binary_crawler.sh.sh
@@ -0,0 +1,7 @@
+
+#rnslab1
+#scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_103/trained_model/*_400.h5  ./../database_seed_50/trained_model/
+
+#rnslab2
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_101/trained_model/*_400.h5  ./../database_seed_50/trained_model/
+

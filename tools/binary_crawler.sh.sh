e0c01fec894b292d0ba66577788d1155887d27cf
salahuddin975 <salahuddin975@gmail.com>
2020-12-13 22:06:00 -0600

    update binary crawler

--- tools/binary_crawler.sh.sh
+++ tools/binary_crawler.sh.sh
@@ -1,7 +1,11 @@
+seed=101
+episode=400
 
 #rnslab1
-#scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_103/trained_model/*_400.h5  ./../database_seed_50/trained_model/
+#scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_${seed}/trained_model/*_${episode}.h5  ./../database_seed_50/trained_model/
+#scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_${seed}/parameters*.txt  ./../database_seed_50/
 
 #rnslab2
-#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_101/trained_model/*_400.h5  ./../database_seed_50/trained_model/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_${seed}/trained_model/*_${episode}.h5  ./../database_seed_50/trained_model/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_${seed}/parameters*.txt  ./../database_seed_50/
 

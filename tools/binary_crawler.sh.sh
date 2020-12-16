39b6f14c21ec1db6bdf8dd7fd5d385a2197d7adc
salahuddin975 <salahuddin975@gmail.com>
2020-12-16 12:05:01 -0600

    add binary and log to copy

--- tools/binary_crawler.sh.sh
+++ tools/binary_crawler.sh.sh
@@ -1,11 +1,16 @@
-seed=101
+seed=131
 episode=400
 
 #rnslab1
 scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_${seed}/trained_model/*_${episode}.h5  ./../database_seed_50/trained_model/
+scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_${seed}/replay_buffer/*  ./../database_seed_50/replay_buffer/
 scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/database_seed_${seed}/parameters*.txt  ./../database_seed_50/
+scp ssalahud@rnslab.hpcc.uh.edu:/home/ssalahud/fire-power/FirePower-agent-private/fire-power-${seed}.log  ./../logs/
+
 
 #rnslab2
 #scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_${seed}/trained_model/*_${episode}.h5  ./../database_seed_50/trained_model/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_${seed}/replay_buffer/*  ./../database_seed_50/replay_buffer/
 #scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/database_seed_${seed}/parameters*.txt  ./../database_seed_50/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/FirePower-agent-private/fire-power-${seed}.log  ./../logs/
 

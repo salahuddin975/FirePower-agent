d9dac1b9c869ba2e999f21d6b4347b7bb197e59e
salahuddin975 <salahuddin975@gmail.com>
2022-02-07 13:51:35 -0600

    use 1.4M experiences to train the agent

--- replay_buffer.py
+++ replay_buffer.py
@@ -9,7 +9,7 @@ class ReplayBuffer:
         self._capacity = buffer_capacity
         self._batch_size = batch_size
         # self._load_replay_buffer_dir = os.path.join(base_path, "replay_buffer")
-        self._load_replay_buffer_dir = os.path.join("2M_experiences")
+        self._load_replay_buffer_dir = os.path.join("1_4M_experiences")
         self._save_replay_buffer_dir = os.path.join(base_path, "replay_buffer")
         self._create_dir()
 

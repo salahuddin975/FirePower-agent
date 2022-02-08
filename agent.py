fc31f3017c54c28fc74762023470c9dd232eacec
salahuddin975 <salahuddin975@gmail.com>
2022-02-07 23:46:36 -0600

    update agent

--- agent.py
+++ agent.py
@@ -229,12 +229,12 @@ class Agent:
         st_bus_branch_fire_distance_comb_layer1 = layers.Dense(128, activation="relu") (st_bus_branch_fire_distance_comb)
 
         st_gen_combine = layers.Concatenate() ([st_gen_output, act_gen_injection])
-        st_gen_layer1 = layers.Dense(32, "relu") (st_gen_combine)
+        st_gen_layer1 = layers.Dense(64, "relu") (st_gen_combine)
 
-        st_line_flow_layer1 = layers.Dense(32, activation="relu") (st_line_flow)
+        st_line_flow_layer1 = layers.Dense(64, activation="relu") (st_line_flow)
 
         st_gen_line_flow_combine = layers.Concatenate() ([st_gen_layer1, st_line_flow_layer1])
-        st_gen_line_flow_layer1 = layers.Dense(64, activation="relu") (st_gen_line_flow_combine)
+        st_gen_line_flow_layer1 = layers.Dense(128, activation="relu") (st_gen_line_flow_combine)
 
         state = layers.Concatenate() ([st_bus_branch_fire_distance_comb_layer1, st_gen_line_flow_layer1])
         # state = layers.Concatenate() ([st_bus_branch_layer1, st_fire_distance_layer1, st_gen_line_flow_layer1])

1c4f2fc50d5ca2ed75ee056d2477e626ab4b41d8
salahuddin975 <salahuddin975@gmail.com>
2022-04-14 17:25:52 -0500

    update

--- agent.py
+++ agent.py
@@ -87,7 +87,7 @@ class Agent:
             target_actor_actions = self._target_actor(next_state_batch)
             # action_batch1.append(target_actions)
             # y = reward_batch[0] + self._gamma * self._target_critic([next_state_batch, target_actor_actions]) * episode_end_flag_batch
-            y = episode_end_flag_batch + self._gamma * self._target_critic([next_state_batch, target_actor_actions]) * episode_end_flag_batch
+            y = 1-episode_end_flag_batch + self._gamma * self._target_critic([next_state_batch, target_actor_actions]) * episode_end_flag_batch
             # y = reward_batch[0] + reward_batch[1] +  reward_batch[2] +  reward_batch[3] +  reward_batch[4]
             critic_value = self._critic([state_batch, action_batch])
             critic_loss = tf.math.reduce_mean(tf.math.square(y - critic_value))
@@ -99,7 +99,7 @@ class Agent:
             actor_actions = self.actor(state_batch)
             # critic_value1 = self._critic([state_batch, actor_actions])
             load_loss = self._total_max_power_gen - tf.reduce_sum(actor_actions * self._max_power_gen, axis=1)
-            critic_value1 = self._critic([state_batch, actor_actions]) * -1650000 + load_loss * -1000
+            critic_value1 = self._critic([state_batch, actor_actions]) * -1000 + load_loss * -1
             actor_loss = -1 * tf.math.reduce_mean(critic_value1)
         actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
         self._actor_optimizer.apply_gradients(zip(actor_grad, self.actor.trainable_variables))

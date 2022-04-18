728345849d5d1110d628aa9fc79df516fe73404e
salahuddin975 <salahuddin975@gmail.com>
2022-04-18 14:48:32 -0500

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from parameters import Parameters
 from agent import Agent
 from replay_buffer import ReplayBuffer
-from data_processor import DataProcessor, Tensorboard, SummaryWriter
+from data_processor import DataProcessor, Tensorboard, SummaryWriter, SummaryWriterStep
 from simulator_resorces import SimulatorResources, Generators
 
 
@@ -123,6 +123,7 @@ if __name__ == "__main__":
 
     tensorboard = Tensorboard(base_path)
     summary_writer = SummaryWriter(base_path, save_model_version, load_episode_num)
+    summary_writer_step = SummaryWriterStep(base_path)
     data_processor = DataProcessor(simulator_resources, generators, state_spaces, action_spaces)
 
     # agent training
@@ -165,6 +166,7 @@ if __name__ == "__main__":
             if explore_network_flag == False:
                 print(f"Episode: {episode}, at step: {step}, reward: {reward[0]}")
             buffer.add_record((state, net_action, reward, next_state, env_action, not done))
+            # summary_writer_step.add_info(episode, step, reward[0], reward[1])
 
             episodic_penalty += reward[0]
             episodic_load_loss += reward[1]

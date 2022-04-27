5b57e2e428509cdef5d728275b4ccf6ef6aa2fb2
salahuddin975 <salahuddin975@gmail.com>
2022-04-27 14:02:09 -0500

    update

--- data_processor.py
+++ data_processor.py
@@ -140,13 +140,13 @@ class DataProcessor:
         nn_output = np.clip(nn_output, 0, 1)
         # print("nn output: ", nn_output)
 
-        # action = {
-        #     "generator_injection": nn_output,
-        # }
-
         action = {
-            "generator_injection": np.zeros(11, int),
+            "generator_injection": nn_output,
         }
+        #
+        # action = {
+        #     "generator_injection": np.zeros(11, int),
+        # }
 
         return action
 

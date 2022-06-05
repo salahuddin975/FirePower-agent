089f57bec91313490ed5e28494f9f3c89c7d70e5
salahuddin975 <salahuddin975@gmail.com>
2022-06-05 16:14:05 -0500

    update

--- fire_propagation_visualizer.py
+++ fire_propagation_visualizer.py
@@ -126,5 +126,7 @@ if __name__ == "__main__":
 
     # images[0].save("map.gif", save_all=True, append_images=images[1:], loop=True)
 
- # ffmpeg -framerate 30 -pattern_type sequence -i 'fire_propagation_0_%d.png' -c:v libx264 -pix_fmt yuv420p episode_0.mp4
- # for i in `seq 0 2`; do ffmpeg -framerate 30 -pattern_type sequence -i fire_propagation_${i}_%d.png -c:v libx264 -pix_fmt yuv420p episode_${i}.mp4 ; done
+# scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/\*.png  .
+
+# ffmpeg -framerate 30 -pattern_type sequence -i 'fire_propagation_0_%d.png' -c:v libx264 -pix_fmt yuv420p episode_0.mp4
+# for i in `seq 0 2`; do ffmpeg -framerate 30 -pattern_type sequence -i fire_propagation_${i}_%d.png -c:v libx264 -pix_fmt yuv420p episode_${i}.mp4 ; done

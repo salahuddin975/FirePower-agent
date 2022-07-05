0b9a85848c038b91bc86427efe916a4a8d8ab20a
salahuddin975 <salahuddin975@gmail.com>
2022-07-05 16:48:38 -0500

    update

--- tools/step_by_step_cumulative_positive_penalty.py
+++ tools/step_by_step_cumulative_positive_penalty.py
@@ -1,18 +1,24 @@
 import pandas as pd
 
-file1 = "step_by_step_test_result_ep_800.csv"
-df1 = pd.read_csv (file1)
+step_by_step = True
+episode = 800
 
-df = pd.DataFrame(columns=["episode", "step", "myopic", "myopic_reward_rl_transition", "rl"])
+if step_by_step:
+    file1 = f"step_by_step_test_result_ep_{episode}.csv"
+    df = pd.DataFrame(columns=["episode", "step", "myopic", "myopic_reward_rl_transition", "rl"])
+else:
+    file1 = f"episodic_test_result_ep_{episode}.csv"
+    df = pd.DataFrame(columns=["episode", "myopic", "myopic_reward_rl_transition", "rl"])
 
 total_myopic = 0
 total_rl_transition = 0
 total_rl = 0
+df1 = pd.read_csv (file1)
 
 for index in range(len(df1)):
     row = df1.iloc[index]
 
-    if int(row["step"]) == 0:
+    if step_by_step and int(row["step"]) == 0:
         total_myopic = 0
         total_rl_transition = 0
         total_rl = 0
@@ -25,13 +31,17 @@ for index in range(len(df1)):
     total_rl_transition += rl_transition
     total_rl += rl
 
-    val = {"episode":row["episode"], "step":row["step"], "myopic":total_myopic,
-           "myopic_reward_rl_transition":total_rl_transition, "rl":total_rl}
+    if step_by_step:
+        val = {"episode":row["episode"], "step":row["step"], "myopic":total_myopic,
+               "myopic_reward_rl_transition":total_rl_transition, "rl":total_rl}
+    else:
+        val = {"episode":row["episode"], "myopic":total_myopic,
+               "myopic_reward_rl_transition":total_rl_transition, "rl":total_rl}
+
     df = df.append(val, ignore_index=True)
-df.to_csv("step_by_step_cumulative_penalty.csv")
 
-
-
-
-
+if step_by_step:
+    df.to_csv(f"step_by_step_cumulative_penalty_ep_{episode}.csv")
+else:
+    df.to_csv(f"episodic_cumulative_penalty_ep_{episode}.csv")
 

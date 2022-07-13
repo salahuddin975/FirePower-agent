02ff99ae79d892c198b3a1725523fa07cb50921f
salahuddin975 <salahuddin975@gmail.com>
2022-07-13 12:53:48 -0500

    add script to calculate average step by step penalty over episodes

---
+++ tools/average_step_by_step_penalty.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+file1 = "step_by_step_test_result_ep_800.csv"
+df1 = pd.read_csv(file1)
+
+df = pd.DataFrame(columns=["step", "myopic", "myopic_reward_rl_transition"])
+
+num_of_episode = 30
+for step in range(300):
+    total_myopic_penalty = 0
+    total_rl_penalty = 0
+    for episode in range(num_of_episode):
+        index = episode * 300 + step
+
+        penalty = float(df1.iloc[index]["myopic"])
+        total_myopic_penalty += penalty
+
+        penalty = float(df1.iloc[index]["myopic_reward_rl_transition"])
+        total_rl_penalty += penalty
+
+    average_myopic_penalty = total_myopic_penalty/num_of_episode
+    average_rl_penalty = total_rl_penalty/num_of_episode
+
+    row = {"step": step, "myopic": average_myopic_penalty, "myopic_reward_rl_transition": average_rl_penalty}
+
+    df = df.append(row, ignore_index=True)
+df.to_csv("average_step_by_step_test_result.csv")
+
+
+
+
+
+
+

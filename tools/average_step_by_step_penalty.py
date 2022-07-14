eb7261273a569b1f5c738def9fd06b1d6190906c
salahuddin975 <salahuddin975@gmail.com>
2022-07-14 13:33:49 -0500

    update

--- tools/average_step_by_step_penalty.py
+++ tools/average_step_by_step_penalty.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-file1 = "step_by_step_test_result_ep_800.csv"
+file1 = "step_by_step_test_result_ep_1920.csv"
 df1 = pd.read_csv(file1)
 
 df = pd.DataFrame(columns=["step", "myopic", "myopic_reward_rl_transition"])
 
-num_of_episode = 30
-myopic_cumulative_penalty = 0
-rl_cumulative_penalty = 0
+num_of_episode = 100
+# myopic_cumulative_penalty = 0
+# rl_cumulative_penalty = 0
 
 for step in range(300):
     total_myopic_penalty = 0
@@ -24,13 +24,13 @@ for step in range(300):
     average_myopic_penalty = total_myopic_penalty/num_of_episode
     average_rl_penalty = total_rl_penalty/num_of_episode
 
-    myopic_cumulative_penalty += average_myopic_penalty
-    rl_cumulative_penalty += average_rl_penalty
+    # myopic_cumulative_penalty += average_myopic_penalty
+    # rl_cumulative_penalty += average_rl_penalty
 
-    row = {"step": step, "myopic": myopic_cumulative_penalty, "myopic_reward_rl_transition": rl_cumulative_penalty}
+    row = {"step": step, "myopic": average_myopic_penalty, "myopic_reward_rl_transition": average_rl_penalty}
     df = df.append(row, ignore_index=True)
 
-df.to_csv("cumulative_average_step_by_step_test_result.csv")
+df.to_csv("average_step_by_step_test_result.csv")
 
 
 

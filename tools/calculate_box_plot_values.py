c169e13a1ddb213a633d5f6013bea623cda2b018
salahuddin975 <salahuddin975@gmail.com>
2022-07-14 13:42:38 -0500

    update

--- tools/calculate_box_plot_values.py
+++ tools/calculate_box_plot_values.py
@@ -2,14 +2,14 @@ import math
 
 import pandas as pd
 
-file1 = "episodic_test_result_ep_800.csv"
+file1 = "episodic_test_result_ep_1920.csv"
 # file1 = "episodic_test_result_ep_800_sp_015.csv"
 # file1 = "episodic_test_result_ep_800_sp_025.csv"
 df1 = pd.read_csv(file1)
 
 df = pd.DataFrame(columns=["step", "myopic", "myopic_reward_rl_transition"])
 
-num_of_episodes = 30
+num_of_episodes = 100
 myopic_penalties = []
 rl_penalties = []
 

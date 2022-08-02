de02e4998c548790a7a731bce0e8ba034c3ea1f3
salahuddin975 <salahuddin975@gmail.com>
2022-08-02 15:03:05 -0500

    fix issue

--- tools/optimal_control_simulation_time_flat.py
+++ tools/optimal_control_simulation_time_flat.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
-file = f"perfect_control_simulation_time.csv"
+file = f"/Users/smsalahuddinkadir/Desktop/test_results/perfect_control/time_limit_900/perfect_control_simulation_time.csv"
 df = pd.read_csv(file)
 
 all_val = []
-for val in df.values.tolist():
-    all_val += val[:300]
-
+for row in df.values.tolist():
+    all_val += row[:100]
 
 df = pd.DataFrame(columns=["all_time"])
 df["all_time"] = all_val

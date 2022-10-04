87e5471c962968dcab3ac1b25d0f8f3c0d30d599
salahuddin975 <salahuddin975@gmail.com>
2022-10-04 16:05:39 -0500

    use relu activation

--- ddpg.py
+++ ddpg.py
@@ -202,7 +202,7 @@ class DDPG:
 
         hidden = layers.Dense(512, activation="relu") (state)
         hidden = layers.Dense(512, activation="relu") (hidden)
-        gen_inj_output = layers.Dense(self._num_of_active_generators, activation="sigmoid") (hidden)
+        gen_inj_output = layers.Dense(self._num_of_active_generators, activation="relu") (hidden)
 
         model = tf.keras.Model([bus_input, branch_input, fire_distance_input, gen_inj_input, load_demand_input, theta_input, line_flow_input],
                                [gen_inj_output])

fix normalization issue

ddpg.py
@@ -51,8 +51,8 @@ class DDPG:
         # self._load_weight_directory = os.path.join("../../FirePower-agent-private", base_path, "trained_model")
         self._create_dir()
 
-        self._actor_optimizer = tf.keras.optimizers.Adam(actor_lr)
-        self._critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
+        self._actor_optimizer = tf.keras.optimizers.Adam(learning_rate=actor_lr)
+        self._critic_optimizer = tf.keras.optimizers.Adam(learning_rate=critic_lr)
 
         self._state_spaces = copy.deepcopy(state_spaces)
         self._action_spaces = copy.deepcopy(action_spaces)
@@ -125,12 +125,21 @@ class DDPG:
             target_critic_values = self._target_critic((target_critic_state, next_state_batch[1])) #* (1 - episode_end_flag_batch)
             return_y = reward_batch + self._gamma * target_critic_values
 
+            # print("target_critic_values:", target_critic_values)
+            # print("return_y:", return_y)
+
             critic_state = tf.concat([state_batch[0], action_batch], axis=-1)
             critic_value_with_original_actions = self._critic((critic_state, state_batch[1]))
             critic_loss = tf.math.reduce_mean(tf.math.square(return_y - critic_value_with_original_actions))
 
+            # print("critic_value_with_original_actions:", critic_value_with_original_actions)
+            # print("critic loss:", critic_loss)
+
+        # print("before: self._critic.trainable_variables:", self._critic.trainable_variables)
         critic_grad = tape.gradient(critic_loss, self._critic.trainable_variables)
+        # print("critic_grad:", critic_grad)
         self._critic_optimizer.apply_gradients(zip(critic_grad, self._critic.trainable_variables))
+        # print("after: self._critic.trainable_variables:", self._critic.trainable_variables)
 
         # update actor network
         with tf.GradientTape() as tape:
@@ -138,13 +147,21 @@ class DDPG:
             critic_state = tf.concat([state_batch[0], actor_actions], axis=-1)
             critic_value_with_actor_actions = self._critic((critic_state, state_batch[1]))
             actor_loss = -1 * tf.math.reduce_mean(critic_value_with_actor_actions)
+            # print("actor_loss:", actor_loss)
 
+        # print("before_optimizer: self.actor.trainable_variables:", self.actor.trainable_variables)
         actor_grad = tape.gradient(actor_loss, self.actor.trainable_variables)
+        # print("actor_grad: ", actor_grad)
         self._actor_optimizer.apply_gradients(zip(actor_grad, self.actor.trainable_variables))
+        # print("after_optimizer: self.actor.trainable_variables:", self.actor.trainable_variables)
 
         # self._update_target()
+        # print("before_target_actor:", self._target_actor.variables)
+        # print("before_target_critic:", self._target_critic.variables)
         self.update_target(self._target_actor.variables, self.actor.variables)
         self.update_target(self._target_critic.variables, self._critic.variables)
+        # print("after_target_actor:", self._target_actor.variables)
+        # print("after_target_critic:", self._target_critic.variables)
 
         return TensorboardInfo(tf.math.reduce_mean(reward_batch), tf.math.reduce_mean(target_actor_actions),
                 tf.math.reduce_mean(target_critic_values), tf.math.reduce_mean(return_y),

graph_convolution.py
@@ -17,18 +17,19 @@ class PaddedOutput(layers.Layer):
 def create_ffn(hidden_units, dropout_rate, name=None):
     fnn_layers = []
     for units in hidden_units:
-        fnn_layers.append(layers.BatchNormalization())
+        # fnn_layers.append(layers.BatchNormalization())
         # fnn_layers.append(layers.Dropout(dropout_rate))
         fnn_layers.append(layers.Dense(units, activation=tf.nn.gelu))
     return tf.keras.Sequential(fnn_layers, name=name)
 
 class GraphConvLayer(layers.Layer):
-    def __init__(self, hidden_units, dropout_rate, normalize, *args, **kwargs,):
+    def __init__(self, hidden_units, dropout_rate, normalize, name, *args, **kwargs,):
 
         super().__init__(*args, **kwargs)
         self.aggregation_type = "sum"
         self.combination_type = "concat"
         self.normalize = normalize
+        self.layer_name = name
 
         self.prepare_neighbor_messages_ffn = create_ffn(hidden_units, dropout_rate)
         # if self.combination_type == "gated":
@@ -45,10 +46,10 @@ class GraphConvLayer(layers.Layer):
     def aggregate_neighbor_messages(self, node_indices, neighbour_messages, num_nodes):
         # node_indices shape is [num_edges], neighbour_messages shape: [num_edges, representation_dim].
         # if self.aggregation_type == "sum":
-        agg = []
-        for neigh in neighbour_messages:      # iterate over batch
-            agg.append(tf.math.unsorted_segment_sum(neigh, node_indices, num_segments=num_nodes))
-        aggregated_message = tf.stack(agg)
+        stk = []
+        for i in range(neighbour_messages.shape[0]):      # iterate over batch
+            stk.append(tf.math.unsorted_segment_sum(neighbour_messages[i], node_indices, num_segments=num_nodes))
+        aggregated_message = tf.stack(stk)
         # aggregated_message = tf.stack(tf.math.unsorted_segment_sum(neigh_message, node_indices, num_segments=num_nodes) for neigh_message in tf.unstack(neighbour_messages))
         # elif self.aggregation_type == "mean":
         #     aggregated_message = tf.math.unsorted_segment_mean(neighbour_messages, node_indices, num_segments=num_nodes)
@@ -63,19 +64,28 @@ class GraphConvLayer(layers.Layer):
         # if self.combination_type == "gru":
         #     h = tf.stack([node_repesentations, aggregated_messages], axis=1)
         # elif self.combination_type == "concat":
-        h = tf.concat([node_repesentations, aggregated_messages], axis=2)
+        # print("node_representation.shape:", node_repesentations.shape)
+        # print("aggregated_messages.shape:", aggregated_messages.shape)
+        stk = []
+        for i in range(node_repesentations.shape[0]):
+            stk.append(tf.concat([node_repesentations[i], aggregated_messages[i]], axis=1))
+        h = tf.stack(stk)
+        # print("h.shape:", h.shape)
         # elif self.combination_type == "add":
         #     h = node_repesentations + aggregated_messages
         # else:
         #     raise ValueError(f"Invalid combination type: {self.combination_type}.")
 
         node_embeddings = self.node_embedding_fn(h)
+        # print("node_embeddings:", node_embeddings.shape)
+        # print(self.layer_name, ": node_embedding_fn weights:", self.node_embedding_fn.get_weights())
         # if self.combination_type == "gru":
         #     node_embeddings = tf.unstack(node_embeddings, axis=1)[-1]
         if self.normalize:
-            node_embeddings = tf.nn.l2_normalize(node_embeddings, axis=-1)
+            node_embeddings = tf.nn.l2_normalize(node_embeddings)
         return node_embeddings
 
+    # @tf.function
     def call(self, inputs):
         node_repesentations, branches, branch_weights = inputs
         node_indices, neighbour_indices = branches[0], branches[1]
@@ -84,7 +94,10 @@ class GraphConvLayer(layers.Layer):
         # print("node_indices:", node_indices.shape)
         # print("neighbour_indices:", neighbour_indices.shape)
         # print("node_representation:", node_repesentations.shape)
-        neighbour_repesentations = tf.gather(node_repesentations, neighbour_indices, axis=1, batch_dims=-1)     # neighbour_repesentations shape is [batch_size, num_edges, representation_dim].
+        stk = []
+        for i in range(node_repesentations.shape[0]):
+            stk.append(tf.gather(node_repesentations[i], neighbour_indices))     # neighbour_repesentations shape is [batch_size, num_edges, representation_dim].
+        neighbour_repesentations = tf.stack(stk)
         # print("neighbour_representations:", neighbour_repesentations.shape)
         neighbour_messages = self.prepare_neighbor_messages(neighbour_repesentations, branch_weights)
         # print("neighbour_messages:", neighbour_messages.shape)
@@ -100,7 +113,7 @@ class GNN_conv(tf.keras.Model):
         hidden_units = [8, 8]
         dropout_rate = 0.2
         normalize = True
-        self.num_conv_layer = 2
+        self.num_conv_layer = 4
 
         branch_path = "configurations/branches.csv"
         branches = pd.read_csv(branch_path, header=None, names=["node_a", "node_b"])
@@ -109,18 +122,9 @@ class GNN_conv(tf.keras.Model):
 
         self.node_feature_processing_ffn = create_ffn(hidden_units, dropout_rate, name="preprocess")
 
-        if self.num_conv_layer >= 1:
-            self.conv1 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv1",)
-        if self.num_conv_layer >= 2:
-            self.conv2 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv2",)
-        if self.num_conv_layer >= 3:
-            self.conv3 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv3",)
-        if self.num_conv_layer >= 4:
-            self.conv4 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv4",)
-        if self.num_conv_layer >= 5:
-            self.conv5 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv5",)
-        if self.num_conv_layer >= 6:
-            self.conv6 = GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv6",)
+        self.conv_layers = []
+        for i in range(self.num_conv_layer):
+            self.conv_layers.append(GraphConvLayer(hidden_units, dropout_rate, normalize, name="graph_conv" + str(i),))
 
         self.node_embedding_processing_ffn = create_ffn(hidden_units, dropout_rate, name="postprocess")
         self.compute_logits = layers.Dense(units=1, name="logits")    # logits layer for actor
@@ -130,35 +134,24 @@ class GNN_conv(tf.keras.Model):
             self.compute_actor_values = layers.Dense(units=generators.get_num_generators(), activation="softmax", name="actor_output")
             self.padded_output = PaddedOutput()
 
+    # @tf.function
     def call(self, graph_info):
         node_info, branch_info = graph_info
         # print("node_info_shape:", node_info.shape, ", branch_info_shape:", branch_info.shape)
+        # print("begin: node_info:", node_info)
+        # print("begin: branch_info:", branch_info)
         x = self.node_feature_processing_ffn(node_info)      # process the node_features to produce node representations.
-        # print("x_shape:", x.shape)
-        # print("node_feature_processing_fnn_weights:", self.node_feature_processing_ffn.get_weights())
+        # print("begin: node_feature_processing_fnn weights:", self.node_feature_processing_ffn.get_weights())
+        # print("begin: preprocessed: ", x)
 
-        if self.num_conv_layer >= 1:
-            x1 = self.conv1((x, self.branches, branch_info))
-            # print("x1_shape:", x1.shape)
-            x = x1 + x                                                    # Skip connection.
-        if self.num_conv_layer >= 2:
-            x2 = self.conv2((x, self.branches, branch_info))
-            x = x2 + x                                                    # Skip connection.
-        if self.num_conv_layer >= 3:
-            x3 = self.conv3((x, self.branches, branch_info))
-            x = x3 + x                                                    # Skip connection.
-        if self.num_conv_layer >= 4:
-            x4 = self.conv4((x, self.branches, branch_info))
-            x = x4 + x                                                    # Skip connection.
-        if self.num_conv_layer >= 5:
-            x5 = self.conv5((x, self.branches, branch_info))
-            x = x5 + x                                                    # Skip connection.
-        if self.num_conv_layer >= 6:
-            x6 = self.conv6((x, self.branches, branch_info))
-            x = x6 + x                                                    # Skip connection.
+        for i in range(self.num_conv_layer):
+            x_out = self.conv_layers[i]((x, self.branches, branch_info))
+            x = x_out + x
 
         x = self.node_embedding_processing_ffn(x)
-        # print("node_embedding_processing weights:", self.node_embedding_processing_ffn.get_weights())
+        # print("end: node_embedding_processing weights:", self.node_embedding_processing_ffn.get_weights())
+        # print("end: x: ", x)
+
         # print("x_shape1:", x.shape)
         # print("x:", x)
         logits = self.compute_logits(x)                       # Compute logits-actor, value-critic
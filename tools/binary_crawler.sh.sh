4ae6de60a3961ebe013ed01796fa41482fe7881a
salahuddin975 <salahuddin975@gmail.com>
2023-05-16 14:15:51 -0500

    update

--- tools/binary_crawler.sh.sh
+++ tools/binary_crawler.sh.sh
@@ -20,12 +20,24 @@ episode=400
 #scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/gams_feasible.csv   .
 #scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/gym-firepower/gym_firepower/envs/gams/temp/\*.zip   .
 
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_4/test_result/episodic_test_result_ep_1960.csv  seed_4/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_5/test_result/episodic_test_result_ep_1920.csv  seed_5/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_6/test_result/episodic_test_result_ep_1920.csv  seed_6/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_7/test_result/episodic_test_result_ep_1600.csv  seed_7/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_8/test_result/episodic_test_result_ep_1460.csv  seed_8/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_9/test_result/episodic_test_result_ep_1620.csv  seed_9/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_10/test_result/episodic_test_result_ep_1400.csv  seed_10/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_11/test_result/episodic_test_result_ep_1400.csv  seed_11/
-scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_12/test_result/episodic_test_result_ep_1400.csv  seed_12/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_1/test_result/episodic_test_result_ep_1460.csv  seed_1/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_2/test_result/episodic_test_result_ep_1500.csv  seed_2/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_3/test_result/episodic_test_result_ep_1240.csv  seed_3/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_4/test_result/episodic_test_result_ep_1240.csv  seed_4/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_5/test_result/episodic_test_result_ep_1200.csv  seed_5/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_6/test_result/episodic_test_result_ep_580.csv  seed_6/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_7/test_result/episodic_test_result_ep_580.csv  seed_7/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_8/test_result/episodic_test_result_ep_1460.csv  seed_8/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_9/test_result/episodic_test_result_ep_500.csv  seed_9/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_10/test_result/episodic_test_result_ep_440.csv  seed_10/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_11/test_result/episodic_test_result_ep_440.csv  seed_11/
+#scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_12/test_result/episodic_test_result_ep_1400.csv  seed_12/
+
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_101/test_result/episodic_test_result_ep_560.csv  seed_101/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_102/test_result/episodic_test_result_ep_560.csv  seed_102/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_103/test_result/episodic_test_result_ep_560.csv  seed_103/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_104/test_result/episodic_test_result_ep_560.csv  seed_104/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_105/test_result/episodic_test_result_ep_740.csv  seed_105/
+scp ssalahud@rnslab2.hpcc.uh.edu:/home/ssalahud/fire_power/remote_compiler/FirePower-agent-private/database_seed_106/test_result/episodic_test_result_ep_760.csv  seed_106/
+
+
